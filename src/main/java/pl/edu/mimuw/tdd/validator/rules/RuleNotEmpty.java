67a06c9dfec5b191f51aae43857f05b6d6afc185
J <a@a.pl>
2011-11-20 17:00:45 +0100

    implementacjaRuleNotEmpty

--- src/main/java/pl/edu/mimuw/tdd/validator/rules/RuleNotEmpty.java
+++ src/main/java/pl/edu/mimuw/tdd/validator/rules/RuleNotEmpty.java
@@ -1,10 +1,12 @@
 package pl.edu.mimuw.tdd.validator.rules;
 
+import org.apache.commons.lang3.StringUtils;
+
 public class RuleNotEmpty implements Rule {
 
 	@Override
 	public boolean isValid(String string) {
-		return false;
+		return !StringUtils.isEmpty(StringUtils.trimToEmpty(string));
 	}
 
 }
