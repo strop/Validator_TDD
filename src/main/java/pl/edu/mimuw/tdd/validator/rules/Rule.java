cad1da9f3ef78e21e5e73408e9fb6083d00f3ddb
J <a@a.pl>
2011-11-20 16:52:07 +0100

    Pierwsze testy

---
+++ src/main/java/pl/edu/mimuw/tdd/validator/rules/Rule.java
@@ -0,0 +1,5 @@
+package pl.edu.mimuw.tdd.validator.rules;
+
+public interface Rule {
+	boolean isValid(String string);
+}
---
+++ src/main/java/pl/edu/mimuw/tdd/validator/rules/RuleNotEmpty.java
@@ -0,0 +1,10 @@
+package pl.edu.mimuw.tdd.validator.rules;
+
+public class RuleNotEmpty implements Rule {
+
+	@Override
+	public boolean isValid(String string) {
+		return false;
+	}
+
+}
