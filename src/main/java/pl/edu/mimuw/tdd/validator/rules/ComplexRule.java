fa06003c98b10cbca3960999d6c3181d0ba9121e
J <a@a.pl>
2011-11-21 12:58:54 +0100

    Testy ComplexRule

---
+++ src/main/java/pl/edu/mimuw/tdd/validator/rules/ComplexRule.java
@@ -0,0 +1,11 @@
+package pl.edu.mimuw.tdd.validator.rules;
+
+public class ComplexRule implements Rule {
+
+	@Override
+	public boolean isValid(String string) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
